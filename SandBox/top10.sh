ab9d6eab73298c72e5e346b57e80b131916b71c9
Beltsazar Anugrah Sotardodo <alternate.3401@gmail.com>
2021-03-13 17:40:49 +0700

    Create SandBox and new script for making new weekly top 10 lists

---
+++ SandBox/top10.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+while getopts w: flag
+do
+	case "${flag}" in 
+		w) wnum=${OPTARG};;
+		*) echo "No week number given!"; exit 1;;
+	esac
+done
+
+if [[ ! $wnum =~ ^[0-1][0-9]$ ]]
+then
+	echo "Bad week number ($wnum)!"; exit 1;
+fi
+
+
+echo -e "---\npermalink: /W$wnum/\n---\n\n#Top 10 List of Week $wnum\nDisclaimer: This list is of no particular orderning, neither of importance nor chronological. It is written in the order in which I remembered them as I was writing this." > "../W$wnum.md"
+
+echo "[W$wnum](https://bltszr.github.io/os211/W$wnum)" >> "../README.md"
