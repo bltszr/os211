6a48a491f20bc52bb0df96f792de54db3b58186b
Beltsazar Anugrah Sotardodo <alternate.3401@gmail.com>
2021-03-20 17:59:10 +0700

    Fix piping

--- SandBox/top10.sh
+++ SandBox/top10.sh
@@ -13,8 +13,7 @@ then
 	echo "Bad week number ($wnum)!"; exit 1;
 fi
 
-cp w_template.md "../w${wnum}.md"
-sed --quiet "s/XX/${wnum}/" "../w${wnum}.md"
+sed "s/XX/${wnum}/" w_template.md > "../w${wnum}.md"
 # echo -e "---\npermalink: /W$wnum/\n---\n\n# Top 10 List of Week $wnum\nDisclaimer: This list is of no particular orderning, neither of importance nor chronological. It is written in the order in which I remembered them as I was writing this." > "../W$wnum.md"
 
 echo "[W$wnum](https://bltszr.github.io/os211/W$wnum)" >> "../README.md"
